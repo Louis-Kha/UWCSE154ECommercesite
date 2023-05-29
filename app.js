962ac4661b414d7e5c0d793de11a3bbe7ea827dc
louiskha <louiskha@uw.edu>
2023-05-29 15:31:45 -0700

    Finished main-view/items endpoint request.

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.use(express.json());
 app.use(multer().none());
 
 /**
+ * Get request endpoint that allows all items from the store to be retrieved.
+ */
+app.get('/main-view/items', async (req, res) => {
+  try {
+    let db = await getDBConnection();
+    let query = "SELECT * FROM store";
+    let results = await db.all(query);
+    res.type('json').send({"store": results});
+
+  } catch (err) {
+    res.type('text').send("An error occurred on the server. Try again later.");
+  }
+});
+
+/**
  * Establishes a database connection to the database and returns the database object.
  * Any errors that occur should be caught in the function that calls this one.
  * @returns {sqlite3.Database} - The database object for the connection.
